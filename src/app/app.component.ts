ba3c0de8f1381eec04cb24a672ae2eb4858c7799
ahmed <ahmed3la@gmail.com>
2021-03-07 22:49:07 +0200

    configer http service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ApiService } from './core/http/api.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myHttpClient';
+
+  constructor(private apiService: ApiService) {
+
+
+  }
+
+  doGet() {
+    this.apiService.doGet();
+  }
+
 }
--- src/app/core/http/api.service.ts
+++ src/app/core/http/api.service.ts
@@ -1,9 +1,21 @@
+//import { environment as env } from './../../../environments/environment';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { environment as env } from '@env/environment';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  doGet() {
+    console.log(env.apiRoot);
+
+  }
+
+
 }
